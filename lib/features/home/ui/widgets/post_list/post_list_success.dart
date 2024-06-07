c437048d5ec931b334928071d65b49f1d11e6995
Fernando Luis <fernando.martinez@soytul.com>
2024-06-06 18:58:28 -0600

    Replace listview with sliverlist

--- lib/features/home/ui/widgets/post_list/post_list_success.dart
+++ lib/features/home/ui/widgets/post_list/post_list_success.dart
@@ -3,42 +3,41 @@ import 'package:tabbar_post_list/features/home/domain/entities/post_entity.dart'
 import 'package:tabbar_post_list/features/home/ui/widgets/post_card/post_card_widgets.dart';
 import 'package:tabbar_post_list/features/home/ui/widgets/post_list/post_list_widgets.dart';
 
-class PostListSuccess extends StatefulWidget {
+class PostListSuccess extends StatelessWidget {
   const PostListSuccess({super.key, required this.postList});
 
   final List<PostEntity> postList;
 
   @override
-  State<PostListSuccess> createState() => _PostListSuccessState();
-}
-
-class _PostListSuccessState extends State<PostListSuccess>
-    with AutomaticKeepAliveClientMixin {
-  @override
   Widget build(BuildContext context) {
-    super.build(context);
-    final childCount = widget.postList.length;
+    final childCount = postList.length;
 
-    return ListView.builder(
-      itemCount: childCount,
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      itemBuilder: (BuildContext context, int index) {
-        final post = widget.postList[index];
-        final isLast = index == (childCount - 1);
+    return CustomScrollView(
+      key: const PageStorageKey<String>('post'),
+      slivers: [
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          sliver: SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                final post = postList[index];
+                final isLast = index == (childCount - 1);
 
-        return Column(
-          children: [
-            // Card
-            PostCard(post: post),
+                return Column(
+                  children: [
+                    // Card
+                    PostCard(post: post),
 
-            // Loading more
-            PostListLoadingMore(show: isLast),
-          ],
-        );
-      },
+                    // Loading more
+                    PostListLoadingMore(show: isLast),
+                  ],
+                );
+              },
+              childCount: childCount,
+            ),
+          ),
+        ),
+      ],
     );
   }
-
-  @override
-  bool get wantKeepAlive => true;
 }
